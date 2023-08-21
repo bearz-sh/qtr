c4cae6be1d49ff991e8cb37d56c21ac91b6d5b4a
bearz-me <119234633+bearz-me@users.noreply.github.com>
2023-08-21 13:49:18 -0500

    bump quasar version to 0.0.3, remove duplicated code in quasar, bump qtr to version 0.0.5

---
+++ deps.ts
@@ -0,0 +1,13 @@
+export * from "https://deno.land/std@0.196.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.196.0/fmt/printf.ts";
+export { parse as parseFlags } from "https://deno.land/std@0.196.0/flags/mod.ts";
+export * as dotenv from "https://deno.land/std@0.196.0/dotenv/mod.ts";
+export * from "https://deno.land/x/quasar@0.0.3/errors/mod.ts";
+export * from "https://deno.land/x/quasar@0.0.3/fmt/host_writer.ts"
+export * from "https://deno.land/x/quasar@0.0.3/text/inflections.ts"
+export * from "https://deno.land/x/quasar@0.0.3/shell/script_runner/mod.ts";
+export * from "https://deno.land/x/quasar@0.0.3/os/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.3/path/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.3/fs/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.3/process/scripting.ts";
+export { PsOutput} from "https://deno.land/x/quasar@0.0.3/process/mod.ts";
