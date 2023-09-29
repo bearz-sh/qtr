6f9249edefa23e05f44250321b3dfbfbe84d9ea5
Michael Herndon <mherndon@solovis.com>
2023-09-29 14:37:46 +0000

    update deps, bump version to 0.0.8

--- deps.ts
+++ deps.ts
@@ -2,12 +2,12 @@ export * from "https://deno.land/std@0.200.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.200.0/fmt/printf.ts";
 export { parse as parseFlags } from "https://deno.land/std@0.200.0/flags/mod.ts";
 export * as dotenv from "https://deno.land/std@0.200.0/dotenv/mod.ts";
-export * from "https://deno.land/x/quasar@0.0.6/errors/mod.ts";
-export * from "https://deno.land/x/quasar@0.0.6/fmt/host_writer.ts"
-export * from "https://deno.land/x/quasar@0.0.6/text/inflections.ts"
-export * from "https://deno.land/x/quasar@0.0.6/shell/script_runner/mod.ts";
-export * from "https://deno.land/x/quasar@0.0.6/os/scripting.ts";
-export * from "https://deno.land/x/quasar@0.0.6/path/scripting.ts";
-export * from "https://deno.land/x/quasar@0.0.6/fs/scripting.ts";
-export * from "https://deno.land/x/quasar@0.0.6/process/scripting.ts";
-export { PsOutput} from "https://deno.land/x/quasar@0.0.6/process/mod.ts";
+export * from "https://deno.land/x/quasar@0.0.7/errors/mod.ts";
+export * from "https://deno.land/x/quasar@0.0.7/fmt/host_writer.ts"
+export * from "https://deno.land/x/quasar@0.0.7/text/inflections.ts"
+export * from "https://deno.land/x/quasar@0.0.7/shell/script_runner/mod.ts";
+export * from "https://deno.land/x/quasar@0.0.7/os/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.7/path/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.7/fs/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.7/process/scripting.ts";
+export { PsOutput} from "https://deno.land/x/quasar@0.0.7/process/mod.ts";
--- mod_info.ts
+++ mod_info.ts
@@ -1,3 +1,3 @@
-export const VERSION = "0.0.7"
+export const VERSION = "0.0.8"
 export const STD_VERSION = "0.200.0"
 export const authors = ["bearz-me"]
