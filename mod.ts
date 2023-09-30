61a3e5e0ec278973f4290d38c5644b0b0091e59d
bearz-me <119234633+bearz-me@users.noreply.github.com>
2023-09-30 14:49:25 -0500

    updating dep versions and bumping versions

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
+export * from "./mod_info.ts";
 export {os, env, fs, ps, path } from "./deps.ts";
 export * from "./pipelines/tasks/task.ts";
 export * from "./pipelines/tasks/shell_task.ts";
--- mod_info.ts
+++ mod_info.ts
@@ -1,1 +1,3 @@
-export const VERSION = "0.0.5"
+export const VERSION = "0.0.6"
+export const STD_VERSION = "0.200.0"
+export const authors = ["bearz-me"]
